2e1f5a7abdd09f4a5dbca9475d90252db0d1129e
Luca Stanger <lstanger@gmx.de>
2020-04-09 15:53:11 +0200

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Luca Stanger on 09.04.20.
+//  Copyright © 2020 Luca Stanger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

41568b446c30bf4f1284b3e2641ba176fe514e36
Luca Stanger <lstanger@gmx.de>
2020-04-11 16:52:34 +0200

    Operation queue

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -6,7 +6,16 @@
 //  Copyright © 2020 Luca Stanger. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 
 enum PhotosResult {
     case success([Photo])
@@ -37,7 +46,9 @@ class PhotoStore {
 //                print("Unexpected error with the request")
 //            }
             let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
@@ -49,4 +60,35 @@ class PhotoStore {
         
         return FlickrAPI.photos(fromJSON: jsonData)
     }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        task.resume()
+    }
+
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else {
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        return .success(image)
+    }
+
 }
+
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -13,6 +13,19 @@ class PhotosViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     var store: PhotoStore!
     
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,6 +35,9 @@ class PhotosViewController: UIViewController {
             switch photosResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
             case let .failure(error):
                 print("Error fetching interesting photos: \(error)")
             }

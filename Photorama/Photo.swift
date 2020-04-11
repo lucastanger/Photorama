3cc8d7309e354381d35b196f71cdeea7c6d97efe
Luca Stanger <lstanger@gmx.de>
2020-04-11 15:36:38 +0200

    json Objekte ausgeben

---
+++ Photorama/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Luca Stanger on 11.04.20.
+//  Copyright © 2020 Luca Stanger. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.photoID = photoID
+        self.remoteURL = remoteURL
+        self.dateTaken = dateTaken
+    }
+    
+}

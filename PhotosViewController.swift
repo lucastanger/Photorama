54c6983010998f118e777f1ccd4722edda18f051
Luca Stanger <lstanger@gmx.de>
2020-04-11 15:00:45 +0200

    Photorama Interface

---
+++ PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Luca Stanger on 09.04.20.
+//  Copyright © 2020 Luca Stanger. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
